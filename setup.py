431442aa5d4c6d3fa14b3bc60aaa9f3d1fcc175b
Thiago Pisani <pisani.thiago@gmail.com>
2014-09-03 22:03:50 -0300

    init cleanup

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="django-thumborize",
+    version="0.0.1",
+    description="",
+    packages=find_packages(),
+    author="Thiago Pisani",
+    author_email="pisani.thiago@gmail.com",
+    url="https://github.com/tpisani/django-thumborize",
+    license="BSD",
+    install_requires=["Django"],
+    classifiers=[
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+    ],
+)
