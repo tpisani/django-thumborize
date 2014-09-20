48cb593d5787ac6c20749ab6ceba45e6d7c02c0d
Thiago Pisani <pisani.thiago@gmail.com>
2014-09-20 12:38:23 -0300

    added thumborize templatetag

---
+++ thumborize/templatetags/thumborize.py
@@ -0,0 +1,12 @@
+from django.template import Library
+
+# Workaround to allow users to import thumborize package
+# and load it's templatetags just about the same way.
+from .. import url
+
+register = Library()
+
+
+@register.simple_tag(name="thumborize")
+def do_thumborize(image_url, **kwargs):
+    return url(image_url, **kwargs)
