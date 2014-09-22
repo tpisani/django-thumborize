1724fa7e4aa34e09194d695a5855a65f65c7376d
Thiago Pisani <pisani.thiago@gmail.com>
2014-09-22 00:21:44 -0300

    bumped version to 0.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="django-thumborize",
-    version="0.0.1",
+    version="0.1",
     description="Provides shortcuts and conveniences for thumbor imaging service",
     packages=find_packages(),
     author="Thiago Pisani",
